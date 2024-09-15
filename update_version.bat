68ac16eac18da1d3b53ac7463a3ee3772750b91a
Johnatas Barbosa <johnatasbarbosa7@gmail.com>
2024-09-15 16:45:24 -0300

    adicionado botão para atualizar programa

---
+++ update_version.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+timeout /t 1 /nobreak >nul
+
+set "origem=api-runner"
+
+set "destino=teste"
+
+xcopy /s /y "%origem%\*" "%destino%\"
+
+if %errorlevel% equ 0 (
+    rmdir /s /q "%origem%"
+    echo Arquivos copiados e arquivos antigos removidos com sucesso.
+) else (
+    echo Ocorreu um erro ao copiar os arquivos.
+)
+
+"Executor de Projetos.exe"
+endlocal
